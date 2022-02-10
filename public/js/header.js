b1a8d304b213b4d32e0fe666ea4f54c5e570f995
pepi1707 <p.angelov99@gmail.com>
2022-02-10 10:37:57 +0200

    html -> js in header

--- public/js/header.js
+++ public/js/header.js
@@ -3,28 +3,31 @@ const header = document.getElementById('header')
 window.addEventListener('load', async (e) => {
     const logged = isLoggedIn()
 
-    title = '––––SHARED CLIPBOARD–––––––––––––––––––––––––––––––––––––'
+    const headerText = document.createElement('h1')
+    headerText.innerText =
+        '––––SHARED CLIPBOARD–––––––––––––––––––––––––––––––––––––'
+    header.replaceChildren(headerText)
 
     if (logged) {
         const notifications = await getNotifications()
-        header.innerHTML = `
-        <h1>
-           ${title}
-            <a href="/profile.html">Profile</a>
-            ${notifications.length}
-        </h1>
-        `
+
+        const profile = document.createElement('a')
+        profile.href = '/profile.html'
+        profile.innerText = 'Profile'
+        header.appendChild(profile)
+
+        const notEl = document.createElement('h3')
+        notEl.innerText = notifications.length
+        header.appendChild(notEl)
     } else {
-        header.innerHTML = `
-        <h1>
-            ${title}
-            <a href="/register.html" class="form-element form-button">
-                Sign up
-            </a>
-            <a href="/login.html" class="form-element form-button">
-                Login
-            </a>
-        </h1>
-    `
+        const signUp = document.createElement('a')
+        signUp.href = '/register.html'
+        signUp.innerText = 'Sign up'
+        header.appendChild(signUp)
+
+        const login = document.createElement('a')
+        login.href = '/login.html'
+        login.innerText = 'Login'
+        header.appendChild(login)
     }
 })
